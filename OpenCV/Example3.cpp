aeadae3188c879ae4c0288fac5e7335b07090e8f
Freeprocessor <wmlee97@naver.com>
2020-07-20 23:41:41 +0900

    Comment

--- OpenCV/Example3.cpp
+++ OpenCV/Example3.cpp
@@ -7,9 +7,12 @@
 using namespace cv;
 using namespace std;
 
+void dummy(int, void*)
+{
 
+}
 
-int threshold1 = 30;
+int threshold1 = 30;// saturation, value 값은 30
 // RGB 색 공간으로는 색의 범위에 따라서 구분을 하기가 힘듬. HVS는 원하는 색을 검출하기 위해 많이 사용되는 색 공간 중 하나
 // 채도는 색상의 진함(H, 0~255), 명도는 색상이 밝은 정도를 의미(V)하고 값이 작아질수록 어두위짐, 
 Vec3b lower_blue1, upper_blue1, lower_blue2, upper_blue2, lower_blue3, upper_blue3;
@@ -34,7 +37,7 @@ void mouse_callback(int event, int x, int y, int flags, void* param)
 		Mat hsv_color;
 		cvtColor(bgr_color, hsv_color, COLOR_BGR2HSV);
 
-		
+		// hue 값을 정수 변수에 대입
 		int hue = hsv_color.at<Vec3b>(0, 0)[0];
 		int saturation = hsv_color.at<Vec3b>(0, 0)[1];
 		int value = hsv_color.at<Vec3b>(0, 0)[2];
@@ -46,7 +49,7 @@ void mouse_callback(int event, int x, int y, int flags, void* param)
 		// 마우스 클릭한 위치에 픽셀에서 얻은 값과 유사한 색상을 추출하기 위해 범위를 정해야 함
 		if (hue < 10)
 		{
-			// 앞에서 구한 6개의 값에 +-10 하여 조정함
+			// 앞에서 구한 6개의 값에 +-10 하여 범위를 조정함
 			cout << "case 1" << endl;
 			lower_blue1 = Vec3b(hue - 10 + 180, threshold1, threshold1);
 			upper_blue1 = Vec3b(180, 255, 255);
@@ -87,12 +90,15 @@ void mouse_callback(int event, int x, int y, int flags, void* param)
 
 int main()
 {
+	// 윈도우 등록
 	namedWindow("img_color");
+	// 윈도우에 마우스 콜백 함수를 등록
 	setMouseCallback("img_color", mouse_callback);
 
 
 	Mat img_hsv;
 
+	// 카메라에서 이미지를 가져올 수 있도록 비디오 캡쳐 색 개체 생성
 	VideoCapture cap(0);
 	if (!cap.isOpened()) {
 
@@ -103,11 +109,15 @@ int main()
 
 	while (1)
 	{
+		// 비디오 캡쳐 객체 리드 함수를 사용하여 카메라로부터 이미지를 가져옴
 		cap.read(img_color);
-
+		// 이미지를 비주얼 색 공간에서 HVS 색 공간으로 변환
 		cvtColor(img_color, img_hsv, COLOR_BGR2HSV);
 
+
+		// 마우스 클릭 시 생성되는 범위를 가지고 HVS 이미지에서 마스크 이미지 생성
 		Mat img_mask1, img_mask2, img_mask3, img_mask;
+		// inRange 함수는 이미지 내에 있는 픽셀을 흰색으로 그 외의 픽셀을 검정색으로 만듬(=바이너리 이미지)
 		inRange(img_hsv, lower_blue1, upper_blue1, img_mask1);
 		inRange(img_hsv, lower_blue2, upper_blue2, img_mask2);
 		inRange(img_hsv, lower_blue3, upper_blue3, img_mask3);
@@ -115,6 +125,7 @@ int main()
 
 
 		Mat img_result;
+		// 마스크 이미지와 원본 이미지를 &연산하여 마스크 이미지에서 흰색으로 표현되는 부분만 원본이미지에서 당겨줌
 		bitwise_and(img_color, img_color, img_result, img_mask);
 
 		imshow("img_color", img_color);
